0ccc7ae29d562431032a85d07ff353fb7d6ec705
khk4912 <30457148+khk4912@users.noreply.github.com>
2020-03-23 20:03:43 +0900

    Fix duplicate log output

--- bot.py
+++ bot.py
@@ -7,15 +7,6 @@ from discord.ext import commands
 
 import json
 
-# 로깅 설정
-logging.getLogger("discord.gateway").setLevel(logging.WARNING)
-logger = logging.getLogger("discord")
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(
-    logging.Formatter("[%(asctime)s] [%(levelname)s] %(name)s: %(message)s")
-)
-logger.addHandler(handler)
 
 # global variables
 # bot infos
@@ -307,6 +298,16 @@ async def on_command_error(ctx: commands.Context, e: Exception):
 
 
 if __name__ == "__main__":
+    # 로깅 설정
+    logging.getLogger("discord.gateway").setLevel(logging.WARNING)
+    logger = logging.getLogger("discord")
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(
+        logging.Formatter("[%(asctime)s] [%(levelname)s] %(name)s: %(message)s")
+    )
+    logger.addHandler(handler)
+
     # print ASCII ART
     with open(file="resources/latte_ascii.txt", mode="rt", encoding="utf-8") as f:
         print(f.read())
